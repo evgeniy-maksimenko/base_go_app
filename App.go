86728389823dcf84f468a4371706568fc10a9107
jeka <siemenspromaster@gmail.com>
2016-01-15 21:10:44 +0200

    init

--- App.go
+++ App.go
@@ -4,25 +4,29 @@ import "fmt"
 import "time"
 
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
-
-	go func() {
-		time.Sleep(time.Second * 1)
-		c1 <- "one"
-	}()
-
+	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(time.Second * 2)
-		c2 <- "two"
+		c1 <- "result 1"
 	}()
 
-	for i := 0; i < 2; i++ {
-		select {
-		case msg1 := <-c1:
-			fmt.Println("reveived", msg1)
-		case msg2 := <-c2:
-			fmt.Println("reveived", msg2)
-		}
+	select {
+	case res:= <-c1:
+		fmt.Println(res)
+	case <-time.After(time.Second * 1):
+		fmt.Println("timeot 1")
+	}
+
+	c2 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "result 2"
+	}()
+
+	select {
+	case res := <-c2:
+		fmt.Println(res)
+	case <-time.After(time.Second * 3):
+		fmt.Println("timeout 2")
 	}
 }
